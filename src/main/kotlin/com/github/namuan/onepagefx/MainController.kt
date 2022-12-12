5d9dea5f28bbf4f1646f516e5220e3fe41205725
namuan <575441+namuan@users.noreply.github.com>
2022-12-12 20:39:33 +0000

    feat: #1 Save notes in an application folder under user home directory

--- src/main/kotlin/com/github/namuan/onepagefx/MainController.kt
+++ src/main/kotlin/com/github/namuan/onepagefx/MainController.kt
@@ -2,9 +2,18 @@ package com.github.namuan.onepagefx
 
 import javafx.fxml.FXML
 import javafx.scene.control.TextArea
+import javafx.scene.input.KeyEvent
+import java.io.File
 
 class MainController {
-
     @FXML
     private lateinit var notesTextArea: TextArea
+
+    fun onKeyPressed(keyEvent: KeyEvent) {
+        val notes = notesTextArea.text
+        val applicationDirectory = System.getProperty("user.home") + "/.onepagefx"
+        val file = File(applicationDirectory, "notes.txt")
+        file.parentFile.mkdirs()
+        file.writeText(notes)
+    }
 }
